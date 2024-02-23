983fc9e6f541b05e91190311a2aa701b49db5571
Furkan Çakıcı <furkancakici25@gmail.com>
2024-02-23 22:19:19 +0300

    feat: next-auth impl.

---
+++ src/lib/auth.ts
@@ -0,0 +1,19 @@
+import { NextAuthOptions } from 'next-auth'
+import { PrismaAdapter } from '@next-auth/prisma-adapter'
+import GoogleProvider from 'next-auth/providers/google'
+import GitHubProvider from 'next-auth/providers/github'
+import prisma from './db'
+
+export const authOption = {
+    adapter: PrismaAdapter(prisma),
+    providers: [
+        GoogleProvider({
+            clientId: process.env.GOOGLE_CLIENT_ID as string,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET as string
+        }),
+        GitHubProvider({
+            clientId: process.env.GITHUB_ID as string,
+            clientSecret: process.env.GITHUB_SECRET as string
+        })
+    ]
+} satisfies NextAuthOptions
