849fcf8969b837767fdf962c717bf9babd37e14d
Furkan Çakıcı <furkancakici25@gmail.com>
2024-03-27 14:09:36 +0300

    chore: remove unused dependencies and update image domains

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['lh3.googleusercontent.com'],
         remotePatterns: [
             {
                 protocol: 'https',
                 hostname: 'image.tmdb.org',
                 port: ''
+            },
+            {
+                hostname: '*.googleusercontent.com'
             }
         ]
     }
